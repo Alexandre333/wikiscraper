7c04affc27a48455452143ef5854e3a21067b27b
Alex <alexandre.meyer333@gmail.com>
2021-09-07 09:11:44 +0200

    Changing license

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 DESCRIPTION = 'Easy scraper that extracts data from Wikipedia articles thanks to its URL slug'
 long_description = (Path(__file__).parent / "README.md").read_text()
 
